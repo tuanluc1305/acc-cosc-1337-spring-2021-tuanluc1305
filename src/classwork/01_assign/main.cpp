d0da5f2d7ddaf56462c123e4742c4a397d1d2a4a
tuanluc1305 <60860583+tuanluc1305@users.noreply.github.com>
2021-05-16 05:40:03 -0500

    Update main.cpp

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -2,11 +2,30 @@
 
 //Write include for capturing input from keyboard and displaying output to screen
 
+#include "types.h"
+#include <iostream>
 
+using std::cin;
+using std::cout;
 
 int main() 
+
 {
+
+	int num;
+	int num1 = 4;
+	int result;
+	
+	cout << "Please enter an integer value: ";
+	cin >> num;
+
+	result = multiply_numbers(num);
+
+	cout << "The result of " << num << " * 5 = " << result << "\n";
+
+	result = multiply_numbers(num1);
+
+	cout << "The result of " << num1 << " * 5 = " << result; 
 
 	return 0;
 }
-
