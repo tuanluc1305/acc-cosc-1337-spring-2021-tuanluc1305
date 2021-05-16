10a7402adf5e9688429c5a0a28c72a0ca9e38e96
tuanluc1305 <60860583+tuanluc1305@users.noreply.github.com>
2021-05-16 04:42:44 -0500

    Update tic_tac_toe_manager.h

--- src/homework/tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,31 @@
-//h
+#ifndef MANAGER
+#define MANAGER
+
+#include "tic_tac_toe.h"
+#include "tic_tac_toe_data.h"
+#include<memory>
+using std::unique_ptr;
+
+class TicTacToeManager 
+
+{
+    friend std::ostream& operator<<(std::ostream& output, const TicTacToeManager& manager);
+
+    public:
+        TicTacToeManager() = default;
+        TicTacToeManager(TicTacToeData& data);
+        void save_game(unique_ptr<TicTacToe>& b);
+        void get_winner_total(int& x, int& o, int& t);
+        ~TicTacToeManager();
+
+    private:
+        vector<unique_ptr<TicTacToe>> games;
+        TicTacToeData data;
+        int x_wins = 0;
+        int o_wins = 0;
+        int ties = 0;
+        void update_winner_count(string winner);
+
+};
+
+#endif
