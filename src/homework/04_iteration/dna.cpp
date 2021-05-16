504ed35d2bddbb2666180b6298a42a88eee32c3b
tuanluc1305 <60860583+tuanluc1305@users.noreply.github.com>
2021-05-16 04:29:48 -0500

    Update dna.cpp

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -6,16 +6,32 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
-
-
-
+double get_gc_content(const string& dna)
+{
+    double cnt = 0;
+    for(std::size_t i=0; i < dna.size(); ++i)
+    {
+        if(dna[i] == 'c' || dna[i] == 'C' || dna[i] == 'g' || dna[i] == 'G')
+        {
+            cnt++;
+        }
+    }
+    return cnt/dna.size();
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
-
-
+string get_reverse_string(string dna)
+{
+    string reverse("");
+    for(int i = dna.size() - 1; i >= 0; --i)
+    {
+        reverse.push_back(dna[i]);
+    }
+    return reverse;
+}
 
 /*
 Write prototype for function get_dna_complement that
@@ -25,6 +41,37 @@ a. call function get_reverse_string(dna), save results to a local string variabl
 b. iterate local string variable and
     replace A with T, T with A, C with G and G with C
 c. return string
-
 */
+string get_dna_complement(string dna)
+{
+    string reverse = get_reverse_string(dna);
 
+    for(auto& c: reverse)
+    {
+        if(c == 'a' || c == 'A')
+        {
+            c = 'T';
+            continue;
+        }
+        
+        else if(c == 't' || c == 'T')
+        {
+            c = 'A';
+            continue;
+        }
+
+        else if(c == 'c' || c == 'C')
+        {
+            c = 'G';
+            continue;
+        }
+
+        else
+        {
+            c = 'C';
+        }
+    }
+    
+    return reverse;
+
+}
