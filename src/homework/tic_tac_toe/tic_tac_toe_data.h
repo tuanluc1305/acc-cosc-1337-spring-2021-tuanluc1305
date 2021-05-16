daef56c6755996ed2a33bf3eecc45ec2b979bd43
tuanluc1305 <60860583+tuanluc1305@users.noreply.github.com>
2021-05-16 04:45:40 -0500

    Update tic_tac_toe_data.h

--- src/homework/tic_tac_toe/tic_tac_toe_data.h
+++ src/homework/tic_tac_toe/tic_tac_toe_data.h
@@ -1,1 +1,22 @@
-//h
+#ifndef DATA_H
+#define DATA_H
+#include "tic_tac_toe.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_4.h"
+#include<vector>
+#include<memory>
+#include<fstream>
+using std::unique_ptr;
+using std::make_unique;
+
+
+class TicTacToeData
+{
+    public:
+        void save_games(const vector<unique_ptr<TicTacToe>>& games);
+        std::vector<std::unique_ptr<TicTacToe>> get_games();
+    private:
+        const string file_name{"tictactoe.dat"};
+};
+
+#endif
