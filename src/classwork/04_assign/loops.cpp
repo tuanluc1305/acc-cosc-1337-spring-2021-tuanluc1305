8aed0029cb86afd227391c250a6b206dbf68f016
tuanluc1305 <60860583+tuanluc1305@users.noreply.github.com>
2021-05-16 05:48:56 -0500

    Update loops.cpp

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,20 @@
 //cpp
+
+#include "loops.h"
+#include <iostream>
+
+using std::cout;
+
+int factorial(int num) 
+{
+
+    auto factorial_result = 1;
+    
+    for(int i=1; i <= num; ++i) 
+    {
+        factorial_result *= i;
+        
+    }
+
+    return factorial_result;
+}
