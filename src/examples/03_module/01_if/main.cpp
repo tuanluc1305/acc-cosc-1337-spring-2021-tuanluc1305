7b7ae4b317e29829dea60f863064af68bf226182
tuanluc1305 <60860583+tuanluc1305@users.noreply.github.com>
2021-05-16 05:46:30 -0500

    Update main.cpp

--- src/examples/03_module/01_if/main.cpp
+++ src/examples/03_module/01_if/main.cpp
@@ -1,22 +1,36 @@
 #include<iostream>
 #include "if.h"//to use is_even function here
+#include <string>
 
+using std::string;
 using std::cout;
 using std::cin;
 
 int main() 
 {
-	//create a bool variable named even
-	//create an int variable named value
+	string dna;
+	int char_count = 0;
+    double string_size =0;
+    double gc_content;
 
-	//prompt user for a keyboard number
-	//assign the number to the value variable
+	cout << "Please enter a DNA string to check: ";
+	cin >> dna;
+   
+    for (unsigned int i = 0; i < dna.size(); ++i) 
+    {
+        if (dna[i] == 'C' || dna[i] == 'G') 
+        {
+            ++char_count;
+			cout << dna[i] << "\n";
+        }
+        ++string_size;
+    }
 
-	//call the is_even function with value as its argument
-	//and assign its return value to the even variable
-	
+    gc_content = char_count / string_size;
+    cout << "size of string is: " << string_size << "\n";
+	cout << "Character count is: " << char_count << "\n";
+    cout << "GC Content is: " << gc_content;
 
-	//display even if number is even
 
 
 	return 0;
