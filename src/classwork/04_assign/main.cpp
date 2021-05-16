3c24ac78ad2d48d03d9a4590149022f3d525ef32
tuanluc1305 <60860583+tuanluc1305@users.noreply.github.com>
2021-05-16 05:44:47 -0500

    Update main.cpp

--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -2,6 +2,12 @@
 
 //write using statements for cin and cout
 
+#include "loops.h"
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +16,45 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	int num;
+	int factorial_num;
+	char quit_program = 'N'; 
+	
+
+	do 
+	{
+		auto input_valid = false;
+		
+		cout << "Please enter an integer between 1 and 10: ";
+		cin >> num;
+		
+		while (input_valid == false) 
+		{
+			if (num < 1 || num > 10) 
+			{
+				cout << "Invalid Entry! \n";
+				cout << "Please enter an integer between 1 and 10: ";
+				cin >> num;
+				input_valid = false;
+			}
+			else 
+			{
+				input_valid = true;
+			}
+		}
+		
+		factorial_num = factorial(num);	
+	
+		cout << "The factorial of " << num << " is " << factorial_num << "\n\n";
+
+		cout << "Do you want to Quit? Y or N: ";
+		cin >> quit_program;
+
+
+	}
+	while(quit_program == 'N' || quit_program =='n');
+
+	
 
 	return 0;
 }
