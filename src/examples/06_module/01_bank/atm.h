32e93093143f91509c68547bf7e0ba203a30e7fd
tuanluc1305 <60860583+tuanluc1305@users.noreply.github.com>
2021-05-16 16:19:44 -0500

    Update atm.h

--- src/examples/06_module/01_bank/atm.h
+++ src/examples/06_module/01_bank/atm.h
@@ -1,1 +1,18 @@
-//atm.h
+#ifndef ATM_H
+#define ATM_H
+
+#include <iostream>
+
+#include "bank_account.h"
+
+class ATM
+{
+public:
+  ATM(BankAccount& a) : account (a){}
+  void display_balance()const;
+
+private: 
+  BankAccount& account;
+};
+
+#endif
