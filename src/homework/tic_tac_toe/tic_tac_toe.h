0f4f29f95c34e1b0694aa84f601683ea64c37ce7
tuanluc1305 <60860583+tuanluc1305@users.noreply.github.com>
2021-05-16 05:31:53 -0500

    Update tic_tac_toe.h

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,50 @@
-//h
+#ifndef GAME
+#define GAME
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using std::string;
+using std::vector;
+using std::cin;
+using std::cout;
+
+class TicTacToe 
+{
+    friend std::ostream& operator<<(std::ostream& output, const TicTacToe& tic_tac_toe);
+    friend std::istream& operator>>(std::istream& input, TicTacToe& tic_tac_toe);
+
+    protected:
+        vector<string> pegs;
+        virtual bool check_column_win();
+        virtual bool check_row_win();
+        virtual bool check_diagonal_win();
+
+    public:
+        TicTacToe(int s): pegs(s*s, " "){}
+        TicTacToe(vector<string> p, string w) : pegs(p), winner(w){}
+        string get_winner();
+        string get_player()const;
+        void start_game(string first_player);
+        void mark_board(int position);
+        bool game_over();   
+        bool peg_empty(int position);  
+        vector<string> get_pegs()const {return pegs;}
+      
+        
+    private:
+        string player;
+        string winner;
+        //vector<string> pegs{9, " "};
+        void clear_board();
+        void set_next_player();
+        bool check_board_full();
+        //bool check_column_win();
+        //bool check_row_win();
+        //bool check_diagonal_win();
+        void set_winner();
+};
+
+#endif // GAME
