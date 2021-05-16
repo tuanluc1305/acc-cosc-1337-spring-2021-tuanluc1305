b4929dbba4b7cf022b62b5e817974adb9611b58c
tuanluc1305 <60860583+tuanluc1305@users.noreply.github.com>
2021-05-16 05:29:10 -0500

    Update tic_tac_toe_4.h

--- src/homework/tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/tic_tac_toe/tic_tac_toe_4.h
@@ -1,1 +1,20 @@
-//h
+#ifndef GAME_4
+#define GAME_4
+
+#include "tic_tac_toe.h"
+
+class TicTacToe4: public TicTacToe 
+{   
+    public:
+        TicTacToe4() : TicTacToe(4){}
+        TicTacToe4(vector<string> p, string winner) : TicTacToe(p, winner){}
+
+
+    private:
+        bool check_column_win() override;
+        bool check_row_win() override;
+        bool check_diagonal_win() override;
+
+};
+
+#endif // GAME_4
